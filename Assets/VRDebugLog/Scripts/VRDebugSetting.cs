809fe83c5e45ad496869ba0cefc075b49fade298
DESKTOP-S74GNT7\krntr <krntrg_krnkrs@yahoo.co.jp>
2017-01-21 10:00:45 +0900

    Foldエディタ拡張を実装

--- Assets/VRDebugLog/Scripts/VRDebugSetting.cs
+++ Assets/VRDebugLog/Scripts/VRDebugSetting.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 using VRDebug;
 
 namespace VRDebug
@@ -16,7 +19,7 @@ namespace VRDebug
         [SerializeField]
         private GameObject oparateController = null;
         public GameObject GetOparateController { get { return oparateController; } }
-        
+
         //GrabContoller
         [SerializeField]
         private GameObject grabController = null;
@@ -38,7 +41,7 @@ namespace VRDebug
 
         void Awake()
         {
-            if(grabController != null)
+            if (grabController != null)
             {
                 if (grabController.GetComponent<VRDebugGrab>() == null)
                 {
@@ -47,4 +50,57 @@ namespace VRDebug
             }
         }
     }
+
+#if UNITY_EDITOR
+
+    [CustomEditor(typeof(VRDebugSetting))]
+    public class MissileEditor : Editor
+    {
+        private SerializedProperty maxLog;
+        private SerializedProperty oparateController;
+        private SerializedProperty grabController;
+        private SerializedProperty grabColliderRadius;
+        private SerializedProperty isBillboard;
+        private SerializedProperty isBind;
+        private bool besicFold = false;
+        private bool controllerFold = false;
+
+        void OnEnable()
+        {
+            maxLog = serializedObject.FindProperty("maxLog");
+            oparateController = serializedObject.FindProperty("oparateController");
+            grabController = serializedObject.FindProperty("grabController");
+            grabColliderRadius = serializedObject.FindProperty("grabColliderRadius");
+            isBillboard = serializedObject.FindProperty("isBillboard");
+            isBind = serializedObject.FindProperty("isBind");
+        }
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            {
+                EditorGUI.indentLevel = 0;
+                besicFold = CustomFold.Foldout("BASIC", besicFold);
+                if(besicFold)
+                {
+                    EditorGUILayout.PropertyField(maxLog, new GUIContent("Max Log"));
+                    EditorGUILayout.PropertyField(isBillboard, new GUIContent("Billboard"));
+                    EditorGUILayout.PropertyField(isBind, new GUIContent("Transform Bind"));
+                }
+                controllerFold = CustomFold.Foldout("CONTROLLER", controllerFold);
+                if(controllerFold)
+                {
+                    EditorGUILayout.PropertyField(oparateController, new GUIContent("Oparate"));
+                    EditorGUILayout.PropertyField(grabController, new GUIContent("Grab"));
+                    if(grabController.objectReferenceValue != null)
+                    {
+                        EditorGUI.indentLevel = 1;
+                        EditorGUILayout.PropertyField(grabColliderRadius, new GUIContent("Collider Radius"));
+                        EditorGUI.indentLevel = 0;
+                    }
+                }
+            }
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+#endif
 }
